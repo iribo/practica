fba973598ede698e8ec85143cbf2e7d33eb824b5
iribo <124275142+iribo@users.noreply.github.com>
2023-02-21 16:07:07 +0200

    Add files via upload

---
+++ 4.appearance.py
@@ -0,0 +1,10 @@
+print("'")
+print("Forsaken by his people, he strode into the wasteland. He traveled far to the north, until he came to the great canyons. There, he founded a small village, Arroyo, where he lived out the rest of his years. And so, for a generation since its founding, Arroyo has lived in peace, its canyons sheltering it from the outside world. It is home. Your home.")
+print("'")
+print("Set your appearance")
+z = input("Male or Female (male/female)?")
+if z=="male":
+    print("You choose male")
+else:
+    print("You choose female")
+    z=male
